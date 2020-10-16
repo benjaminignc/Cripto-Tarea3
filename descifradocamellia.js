2cf87a71cbb3cc1eeb5547516436ec80e5e17133
benjaminignc <benjamin.contrerass@mail.udp.cl>
2020-10-16 14:31:41 -0300

    commit on 'index'

--- descifradocamellia.js
+++ descifradocamellia.js
@@ -3,9 +3,11 @@
 // @namespace    http://tampermonkey.net/
 // @version      0.1
 // @description  try to take over the world!
-// @author       You
+// @author       benjaminignc
 // @match        https://benjaminignc.github.io/Cripto-Tarea3/index.html
 // @grant        none
+// @updateURL    https://raw.githubusercontent.com/benjaminignc/Cripto-Tarea3/index/descifradocamellia.js
+// @downloadURL  https://raw.githubusercontent.com/benjaminignc/Cripto-Tarea3/index/descifradocamellia.js
 // ==/UserScript==
 
 (function() {
