b6931e4e8056d55b239ad6db4a68ee0e3b3c4e79
benjaminignc <benjamin.contrerass@mail.udp.cl>
2020-10-16 16:38:43 -0300

    commit on 'index'

--- descifradocamellia.user.js
+++ descifradocamellia.user.js
@@ -8,7 +8,6 @@
 // @grant        none
 // @updateURL    https://raw.githubusercontent.com/benjaminignc/Cripto-Tarea3/index/descifradocamellia.user.js
 // @downloadURL  https://raw.githubusercontent.com/benjaminignc/Cripto-Tarea3/index/descifradocamellia.user.js
-// @require      https://raw.githubusercontent.com/F4stHosting/F4st_Crypt/master/index.js
 // ==/UserScript==
 
 (function() {
