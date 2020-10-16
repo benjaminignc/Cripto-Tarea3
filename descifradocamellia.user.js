50f6f0a3ca245691e02f6d585d31574be127ecae
benjaminignc <benjamin.contrerass@mail.udp.cl>
2020-10-16 15:00:28 -0300

    commit on 'index'

---
+++ descifradocamellia.user.js
@@ -0,0 +1,17 @@
+// ==UserScript==
+// @name         Hola
+// @namespace    http://github.com/benjaminignc
+// @version      0.1
+// @description  try to take over the world!
+// @author       Benjamín Contreras
+// @match        https://benjaminignc.github.io/Cripto-Tarea3/index.html
+// @grant        none
+// @updateURL    https://raw.githubusercontent.com/benjaminignc/Cripto-Tarea3/index/descifradocamellia.user.js
+// @downloadURL  https://raw.githubusercontent.com/benjaminignc/Cripto-Tarea3/index/descifradocamellia.user.js
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    // Your code here...
+})();
