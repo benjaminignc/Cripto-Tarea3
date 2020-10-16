be9cdd12ada2c329ec8be8bc3803ded3bfa7d183
benjaminignc <benjamin.contrerass@mail.udp.cl>
2020-10-16 16:43:33 -0300

    commit on 'index'

--- descifradocamellia.user.js
+++ descifradocamellia.user.js
@@ -1,5 +1,5 @@
 // ==UserScript==
-// @name         Descifrado Camellia
+// @name         Descifrador Camellia
 // @namespace    http://github.com/benjaminignc
 // @version      0.2
 // @description  Cifrado Simetrico
@@ -8,10 +8,12 @@
 // @grant        none
 // @updateURL    https://raw.githubusercontent.com/benjaminignc/Cripto-Tarea3/index/descifradocamellia.user.js
 // @downloadURL  https://raw.githubusercontent.com/benjaminignc/Cripto-Tarea3/index/descifradocamellia.user.js
+// @require      https://raw.githubusercontent.com/F4stHosting/F4st_Crypt/master/index.js
 // ==/UserScript==
 
 (function() {
     'use strict';
+    var f4cry = require("F4st_Crypt");
     var camellia=document.getElementsByClassName('msg')[0].id
     alert(camellia);
 })();
